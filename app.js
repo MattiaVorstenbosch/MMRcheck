cd7bd69bf069474d2b6175e2d0ded34b90fd3dd3
MattiaVorstenbosch <hello@mattiavorstenbosch.nl>
2018-02-04 23:37:45 +0100

    title

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const steamAvatar = document.querySelector('#steamAvatar');
 const soloMMR = document.querySelector('#soloMMR');
 // display teamMMR
 const teamMMR = document.querySelector('#teamMMR');
+//display
+const estimateMMR = document.querySelector('#estimateMMR');
 // playerTag
 const tagName = document.querySelector('#tagName');
 // playerLink
@@ -25,14 +27,18 @@ document.querySelector('#steamForm').addEventListener('submit', (e) => {
     if(inputValue != '') {
         let steamID = inputValue.value;
         // Get Users
-        const user = http.get('https://api.opendota.com/api/players/' + inputValue + '')
+        // const user = http.get('https://api.opendota.com/api/players/' + inputValue + '')
+        const user = http.get('https://api.opendota.com/api/live')
             .then(data => {
+                console.log(data);
                 soloMMR.innerHTML = data.solo_competitive_rank;
                 teamMMR.innerHTML = data.competitive_rank;
+                estimateMMR.innerHTML = data.mmr_estimate.estimate;
                 steamAvatar.src = data.profile.avatarfull;
                 tagName.innerHTML = data.profile.personaname;
                 playerLink.href = data.profile.profileurl;
                 console.log(data);
+
             })
             .catch(err => soloMMR.innerHTML = 'Something went wrong');
 
